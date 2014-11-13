ebddd8191934d7408bf98db3da0a9617ebff26bd
ruchiraw <ruchira.wageesha@gmail.com>
2014-11-13 06:09:40 -0600

    updating

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var Token = require('token');
+var mongoose = require('mongoose');
+var Token = mongoose.model('Token');
 
 module.exports = function (options) {
     return function (req, res, next) {
