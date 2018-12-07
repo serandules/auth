4337d298a8f26206c8fb916c2b38db596aaef2ff
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-12-08 02:30:14 +0530

    updating

--- index.js
+++ index.js
@@ -1,53 +1,77 @@
 var log = require('logger')('auth');
 var errors = require('errors');
 
-var Token = require('model-tokens');
+var Tokens = require('model-tokens');
+var Otps = require('model-otps');
 require('model-clients');
 require('model-users');
 require('model-tiers');
 
 module.exports = function (options) {
-    options = options || {};
-    return function (req, res, next) {
-        var o;
-        var i;
-        var length;
-        var path = req.path;
-        var auth = req.headers['authorization'];
-        if (!auth) {
-            o = options[req.method];
-            if (o) {
-                length = o.length;
-                for (i = 0; i < length; i++) {
-                    if (new RegExp(o[i], 'i').test(path)) {
-                        return next();
-                    }
-                }
-            }
-            return res.pond(errors.unauthorized())
+  options = options || {};
+  return function (req, res, next) {
+    var otp = req.headers['x-otp'];
+    if (otp) {
+      Otps.findOne({
+        value: otp
+      }).populate('user').exec(function (err, otp) {
+        if (err) {
+          log.error('otps:find-one', err);
+          return next(errors.serverError());
         }
-        var match = /^\s*Bearer\s+(.*)$/g.exec(auth);
-        if (!match) {
-            return res.pond(errors.unsupportedAuth());
+        if (!otp || !otp.user) {
+          return next(errors.unauthorized());
         }
-        var token = match[1];
-        Token.findOne({
-            access: token
-        }).populate('client user tier')
-            .exec(function (err, token) {
-                if (err) {
-                    log.error('tokens:find-one', err);
-                    return res.pond(errors.serverError());
-                }
-                if (!token || !token.user) {
-                    return res.pond(errors.unauthorized());
-                }
-                if (token.accessibility() === 0) {
-                    return res.pond(errors.unauthorized());
-                }
-                req.token = token;
-                req.user = token.user;
-                next();
-            });
-    };
+        Otps.remove({_id: otp.id}, function (err) {
+          if (err) {
+            return next(err);
+          }
+          req.otp = otp;
+          req.user = otp.user;
+          next();
+        });
+      });
+      return;
+    }
+    var auth = req.headers['authorization'];
+    if (auth) {
+      var match = /^\s*Bearer\s+(.*)$/g.exec(auth);
+      if (!match) {
+        return next(errors.unsupportedAuth());
+      }
+      var token = match[1];
+      Tokens.findOne({
+        access: token
+      }).populate('client user tier')
+        .exec(function (err, token) {
+          if (err) {
+            log.error('tokens:find-one', err);
+            return next(errors.serverError());
+          }
+          if (!token) {
+            return next(errors.unauthorized());
+          }
+          if (token.accessibility() === 0) {
+            return next(errors.unauthorized());
+          }
+          req.token = token;
+          req.user = token.user;
+          next();
+        });
+      return;
+    }
+    var i;
+    var length;
+    var path = req.path;
+    var o = options[req.method];
+    if (o) {
+      length = o.length;
+      for (i = 0; i < length; i++) {
+        if (new RegExp(o[i], 'i').test(path)) {
+          return next();
+        }
+      }
+    }
+    return next(errors.unauthorized());
+  };
 };
